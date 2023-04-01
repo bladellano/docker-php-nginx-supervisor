637bc3700737e7916ab3d3240ef449e385ce8687
Caio <bladellano@gmail.com>
2023-03-31 23:25:47 -0300

    Implementando laravel/horizon

--- application/routes/web.php
+++ application/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use App\Jobs\CreateUserJob;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('users/total', function () {
+    return response()->json(['users'=>['total'=>User::all()->count()]]);
+});
+
+Route::get('users/job', function () {
+    CreateUserJob::dispatch();
+    return response()->json(['msg'=>'Job dispachado com sucesso!']);
+});
+
+Route::get('users/job-multiple', function () {
+    $qtdJobs = 100;
+
+    for ($i=0; $i < $qtdJobs; $i++) {
+        CreateUserJob::dispatch()->onQueue('default');
+    }
+    return response()->json(['msg'=>'Jobs dispachados com sucesso!']);
+});
+
+Route::get('users/job-delay', function () {
+    CreateUserJob::dispatch()->delay(now()->addMinute());
+    return response()->json(['msg'=>'Job dispachado e agendado com sucesso!']);
 });
